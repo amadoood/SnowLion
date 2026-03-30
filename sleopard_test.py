8886545e6ffc2782bdc1e570d5809a0ffc0c8054
Khosro Moeini <khosro@bu.edu>
2026-03-30 19:15:30 -0400

    add filedata_id to .env

--- sleopard_test.py
+++ sleopard_test.py
@@ -5,11 +5,12 @@ import os
 load_dotenv() 
 api_key = os.getenv("API_KEY")
 print(api_key)
+datafile_id = os.getenv("DATAFILE_ID")
 
 
 client = SnowLeopardClient(api_key=api_key)
 response = client.retrieve(
-    datafile_id="20c7ab97fb534efaa9ec8c461135e0dc",
+    datafile_id=datafile_id,
     user_query="Give me top 20 songs by Billie Eilish"
 )
 print(response.data[0].rows)
